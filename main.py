b281f2ffb20cf3283f79aca24d7faa2c1508394f
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-23 16:34:44 +0900

    faet: 상위 DQN 구현

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # uvicorn main:app --reload
 from fastapi import FastAPI
-from app.api.recommend import router as recommend_router
+from api.recommend_router import router as recommend_router
 
 app = FastAPI(title="Stock-Aware Recommendation API")
+
+app.include_router(recommend_router)
